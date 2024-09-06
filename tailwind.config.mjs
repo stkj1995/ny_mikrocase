8d0a214c2ac9c1a9e43301cd53ac2b710592d1d5
Sophie <soph1155@stud.kea.dk>
2024-09-06 15:48:33 +0200

    ændringer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}, // Tilføj tilpasninger her, hvis nødvendigt
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
